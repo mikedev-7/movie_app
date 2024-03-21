f27bfe5e5ea1ee2fa5fc8ffe33846fd151a251f2
Michael Ofagbor <eihmenormike@gmail.com>
2024-03-21 09:55:15 -0700

    Updating app router and folder structure

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <header>
+    <router-link to="/">
+      <h1> <span>Vue</span> Movies</h1>
+    </router-link>
+  </header>
+  <main>
+    <router-view/>
+  </main>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+ *{ 
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  &::selection{
+    background: transparentize(#42ba83, 0.5);
   }
+}
+body {
+  background-color: #35495E
 }
 </style>
---
+++ src/env.js
@@ -0,0 +1,3 @@
+export default {
+    apikey: "ce00c510"
+}
